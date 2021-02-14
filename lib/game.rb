0bf439996cd7a3e0247d337744b7a799e4bfb3d3
kevn7n <kevn_nguyen@yahoo.com>
2021-02-13 19:24:10 -0700

    Added Instrucitons option

--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,6 @@ class Game
     @pre_play_input = pre_play_input
     @turn_counter = 0
     @timer = Timer.new #(start_time)
-    # @code = Code.new(4, "Red", "Blue", "Green", "Yellow")
   end
 
   def end_game
@@ -19,47 +18,58 @@ class Game
     run_time = (@timer2.end_time - @timer1.start_time).to_i
     time_in_min = run_time / 60
     time_in_sec = run_time - (time_in_min * 60)
-    print run_time
-    require "pry"; binding.pry
-    puts "Congratulations! You guessed the sequence #{@turn.guess_input} in #{@turn_counter} guess(es) over #{time_in_min} minutes, #{time_in_sec} seconds." #break time into seconds
-    puts "Do you want to (p)lay again or (q)uit?"
-    play_again?
+    # require "pry"; binding.pry
+    puts "Congratulations! You guessed the sequence #{@turn.guess_input} in #{@turn_counter} guess(es) over #{time_in_min} minutes, #{time_in_sec} seconds."
+    puts "Please enter (p)lay again or (q)uit"
+    # play_again?
+    replay_input = gets.chomp
+    if replay_input == "p"
+      start_welcome
+    elsif replay_input == "q"
+      quit
+    # else
+    end
   end
 
   def instructions
-    puts "insert directions here"
+    puts "INSTRUCTIONS:  How to Play Mastermind"
+    puts " "
+    puts "The computer makes a secret pattern of four color code pegs. Duplicates colors are allowed (no blank or empty peg holes)."
+    puts " "
+    puts "The player tries to guess the pattern, in both order and color. The player types the letters in the desired order to represent their guess, and presses enter. Once entered, the computer provides feedback by stating a summary of how many correct elements (correct color) and correct positions (correct color and position)."
+    puts " "
+    puts "If there are duplicate colors in the guess, they cannot all be determined to be a correct element. unless they correspond to the same number of duplicate colors in the hidden code. For example, if the hidden code is red-red-blue-blue and the player guesses red-red-red-blue, the feedback will state 0 correct elements and 3 correct positions [since the red-red-__-blue are all 3 correct colors in correct position], nothing for the third red as there is not a third red in the code."
+    puts " "
+    puts "Once feedback is provided, another guess is made; guesses and feedback continue to alternate until either the player guesses correctly, or concedes by calling to see the secret code [press (c)] or quits [press (q)].  Have fun!"
+    puts " "
+    puts "Press Enter to Continue"
+
+    instructions_input = gets.chomp
+
+    start_welcome if instructions_input = ""
   end
 
-  # def go_guess
-  #   turn = Turn.new
-  #   turn.get_guess
-  # end
-
   def play
-    turn = Turn.new(@code.secret_code.join)
+    turn = Turn.new(@code.secret_code)
     # require "pry"; binding.pry
 
-    # timer = Timer.new #(start_time)
-    # timer.start_time
     @turn_counter += 1
     @turn.get_guess
-    end_game if @turn.guess_input == @code.secret_code.join
-    # @turn.valid_input?
-    # @turn.zero_correct
+    end_game if @turn.guess_input == @code.secret_code
+    # play if @turn.guess_input.class != String && @turn.guess_input.length == 4
     @turn.check_positions_colors
     @turn.show_guess_results
     play
   end
 
   def play_again?
-    replay_input = gets.chomp
-    if replay_input == "p"
-      start_welcome
-    elsif replay_input == "q"
-      quit
-    else
-      puts "Please enter (p)lay again or (q)uit"
-    end
+    # replay_input = gets.chomp
+    # if replay_input == "p"
+    #   start_welcome
+    # elsif replay_input == "q"
+    #   quit
+    # # else
+    # end
     # start_welcome
   end
 
@@ -69,7 +79,7 @@ class Game
     @timer1.start_time
     @code = Code.new(4, "Red", "Blue", "Green", "Yellow")
     @code.make_secret_code
-    @turn = Turn.new(@code.secret_code.join)
+    @turn = Turn.new(@code.secret_code) #.join)
     require "pry"; binding.pry
     puts "I have generated a beginner sequence with four elements made up of:"
     puts "(r)ed,(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
@@ -89,16 +99,19 @@ class Game
     # turn.zero_correct
     @turn.check_positions_colors
     # require "pry"; bindexiting.pry
-    end_game if @turn.guess_input == @code.secret_code.join
+    end_game if @turn.guess_input == @code.secret_code
     @turn.show_guess_results
     play
   end
 
   def quit
-    puts "You are now exiting the game"
-    puts "One last gaem, play again? Please enter (y)es and (n)o."
-    quit_input = gets.chomp
-    start if quit_input == "y"
+    puts "You are now exiting the game.  Please press Enter to go to terminal."
+    # quit_input = gets.chomp
+    # puts "One last gaem, play again? Please enter (y)es and (n)o."
+    # start_welcome if quit_input == "y"
+    # abort
+    # exit
+    # puts "Bye"
   end
 
   def start_welcome
--- lib/turn.rb
+++ lib/turn.rb
@@ -42,21 +42,12 @@ class Turn
     end
   end
 
-  # def check_solution    ##MOVED TO GAME.rb
-  #   @guess_input == @secret_code
-  # end
-
   def get_guess
     puts "Please enter a guess. If you want to quit, press 'q' or press 'c' for the solution."
-    @guess_input = gets.chomp
+    @guess_input = gets.chomp.upcase
     @counter += 1
-    quit if @guess_input == "q"
+    # quit if @guess_input == "q"
     show_cheat_answer if @guess_input == "c"
-    # game.generate_solution    ##MOVED TO GAME.rb
-    # check_solution
-    # valid_input?
-    # zero_correct
-    # check_positions_colors
   end
 
   def show_cheat_answer
@@ -71,30 +62,9 @@ class Turn
     puts "You've taken #{@counter} guess."
   end
 
-  # def valid_input?
-  #   #check quess.length == 4 && correct letter
-  #   # if @game.pre_play_input.length != 4
-  #   #   puts "Please insert a valid guess."
-  #   #   get_guess
-  #   elsif @guess_input.length != 4
-  #     puts "Please insert a valid guess."
-  #     get_guess
-  #   else
-  #     check_positions_colors
-  #   end
-  # end
   def turn_look
     p "hi"
     # require "pry"; binding.pry
   end
 
-  def zero_correct
-    #first check, if no match, go direct to next guess
-    if game.pre_play.secret_code.include?(@guess_input[0]) == false && game.pre_play.secret_code.include?(@guess_input[1]) == false && game.pre_play.secret_code.include?(@guess_input[2]) == false && game.pre_play.secret_code.include?(@guess_input[3]) == false
-      puts "You got zero correct, please guess again."
-      @turn_counter += 1
-      get_guess
-    #   break
-    end
-  end
 end
