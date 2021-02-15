4c40f992dd8b2c4ce1e7bcf048f01513f8de7cd6
kevn7n <kevn_nguyen@yahoo.com>
2021-02-14 22:48:11 -0700

    Updated with color, pre-pick-difficulty

--- lib/game.rb
+++ lib/game.rb
@@ -1,102 +1,128 @@
+require 'rainbow'
+
 class Game
   attr_reader :answer,
               :secret_code,
-              :run_time
+              :run_time,
+              :turn_counter
 
   def initialize (secret_code = nil)
     @secret_code = secret_code
-    @run_time = 0
+    @turn_counter = 0
   end
 
   def end_game
-    @timer.end_timer
-    p "Congratulations! You guessed the sequence #{answer} in #{turn_counter} over #{@timer.total_time}" #break time into seconds
-    p "Do you want to (p)lay again or (q)uit?"
+    print Rainbow("CONGRATUALATIONS!  ").mediumaquamarine.blink.bold
+    puts Rainbow("You guessed the sequence #{@code.secret_code} in #{@turn_counter} guess(es) over " + @timer.total_time).mediumaquamarine.bold
     play_again?
   end
 
-  def generate_solution
-    #randomly generate 4 color solution string
-    colors = ['y', 'r', 'g', 'b']
-    4.times do
-      @answer << colors.sample
-    end
-    @answer
-  end
-
-  # def generate_solution
-  #   colors_positions = {  'y' => 1,
-  #                         'r' => 2,
-  #                         'g' => 3,
-  #                         'b' => 4
-  #                       }
-  #     colors_positions.each do |color,position|
-  #       @answer.push(color, position).shuffle!
-  #     end
-  # end
-
   def instructions
-    p "insert directions here"
-  end
+    puts Rainbow("INSTRUCTIONS:  How to Play Mastermind").silver.blink.bold
+    puts Rainbow(<<-The).lightskyblue
 
-  # def go_guess
-  #   turn = Turn.new
-  #   turn.get_guess
-  # end
+    The computer makes a secret pattern of four color code pegs. Duplicates colors are allowed (no blank or empty peg holes).
+
+    The player tries to guess the pattern, in both order and color. The player types the letters in the desired order to represent their guess, and presses enter. Once entered, the computer provides feedback by stating a summary of how many correct elements (correct color) and correct positions (correct color and position).
+
+    If there are duplicate colors in the guess, they cannot all be determined to be a correct element. unless they correspond to the same number of duplicate colors in the hidden code. For example, if the hidden code is red-red-blue-blue and the player guesses red-red-red-blue, the feedback will state 0 correct elements and 3 correct positions [since the red-red-__-blue are all 3 correct colors in correct position], nothing for the third red as there is not a third red in the code.
+
+    Once feedback is provided, another guess is made; guesses and feedback continue to alternate until either the player guesses correctly, or concedes by calling to see the secret code [press (c)] or quits [press (q)].  Have fun!
+
+    Press Enter to Continue
+    The
+
+    instructions_input = gets.chomp
+
+    start_welcome if instructions_input = ""
+  end
 
   def play
-    turn = Turn.new(@secret_code)
-    #START run_time
-    @timer.start_time
-    turn.get_guess
-    #OR turn.end_game if turn.check_solution
-    turn.end_game if turn.guess_input == @secret_code
-    turn.valid_input?
-    turn.zero_correct
-    turn.check_positions_colors
-    turn.show_guess_results
+    # require "pry"; binding.pry
+    turn = Turn.new(@code.secret_code)
+    @turn_counter += 1
+    @turn.get_guess
+    # qutt if @turn.guess_input == "Q"
+    end_game if @turn.guess_input == @code.secret_code
+    @turn.check_positions_colors
+    @turn.show_guess_results
     play
   end
 
   def play_again?
+    puts Rainbow("Please enter (p)lay again or (q)uit").orange.bold
     replay_input = gets.chomp
     if replay_input == "p"
-      start
+      start_welcome
     elsif replay_input == "q"
       quit
-    else
-      p "Please enter (p)lay again or (q)uit"
     end
-    play_again
   end
 
-  def quit
-    p "You are now exiting the game"
-    p "Do you want to play again? Please enter 'y' for yes and 'n' for no."
-    quit_input = gets.chomp
-    start if quit_input == "y"
-  end
-
-  # def welcome
-  #
-  # end
-
-  def start_game
-    p "Welcome to MASTERMIND"
-    p "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-    p ">"
+  def choose_difficulty
+    puts Rainbow("          Would you like to (p)lay, read the (i)nstructions, or (q)uit?").lightskyblue.bold
+    print Rainbow("<<>>  ").lightskyblue.bold
 
     user_input = gets.chomp
 
+    puts "\n\n"
     if user_input == "q"
       quit
     elsif user_input == "i"
       instructions
     elsif user_input == "p"
+      pre_play
       play
     else
-      p "Invalid input."
-      start
+      puts "Invalid input."
+      start_welcome
+    end
+  end
+
+  def pre_play
+    # turn = Turn.new   #(@code.secret_code)
+    @timer = Timer.new #(start_time)
+    @timer.start_time
+    @code = Code.new(4, "Red", "Blue", "Green", "Yellow")
+    @code.make_secret_code
+    @turn = Turn.new(@code.secret_code) #.join)
+    require "pry"; binding.pry
+    puts Rainbow("I have generated a beginner sequence with four elements made up of:").lightskyblue
+    puts Rainbow("(r)ed,(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.\n").lightskyblue
+  end
+
+  def quit
+    puts Rainbow("You are now exiting the game. See you again soon.\n\n\n\n\n\n\n\n").orange.blink.bold
+    exit
+  end
+
+  def start_welcome
+    puts "\e[2J\e[f"
+    puts Rainbow("                 <><><><><><><>  Welcome to  <><><><><><><><>\n\n").lightskyblue.bold
+    puts Rainbow("███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗██████╗ ").cyan.blink
+    puts Rainbow("████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗").white.blink
+    puts Rainbow("██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║██║  ██║").cyan.blink
+    puts Rainbow("██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██║  ██║").white.blink
+    puts Rainbow("██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██████╔╝").cyan.blink
+    puts Rainbow("╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ \n\n\n\n").white.blink
+
+
+    puts Rainbow("          Would you like to (p)lay, read the (i)nstructions, or (q)uit?").lightskyblue.bold
+    print Rainbow("<<>>  ").lightskyblue.bold
+
+    user_input = gets.chomp
+
+    puts "\n\n"
+    if user_input == "q"
+      quit
+    elsif user_input == "i"
+      instructions
+    elsif user_input == "p"
+      pre_play
+      play
+    else
+      puts "Invalid input."
+      start_welcome
     end
   end
 end
--- lib/turn.rb
+++ lib/turn.rb
@@ -1,92 +1,67 @@
+require 'rainbow'
 
 class Turn
-  attr_reader :board, :turn_counter, :pegs_array, :guess_input
+  attr_reader :guess_input, :counter, :pegs_array
 
   def initialize(secret_code)
     @secret_code = secret_code
-    @turn_counter = 0
+    @counter = 0
     @pegs_array = []
+    @results_array = [] #NEW
   end
-
   def check_positions_colors
     @pegs_array = []  #reset
-    if @secret_code[0] == @guess_input[0]
-      @pegs_array << "red"
-      @pegs_array << "white"
+    (0..3).each do |index|
+      @pegs_array << "red" if @secret_code[index] == @guess_input[index]
     end
-    if @secret_code[1] == @guess_input[1]
-      @pegs_array << "red"
-      @pegs_array << "white"
-    end
-    if @secret_code[2] == @guess_input[2]
-      @pegs_array << "red"
-      @pegs_array << "white"
-    end
-    if @secret_code[3] == @guess_input[3]
-      @pegs_array << "red"
-      @pegs_array << "white"
-    end
-    if @secret_code[0] == @guess_input[1] || @secret_code[0] == @guess_input[2] || @secret_code[0] == @guess_input[3]
-      @pegs_array << "white"
-    end
-    if @secret_code[1] == @guess_input[0] || @secret_code[1] == @guess_input[2] || @secret_code[1] == @guess_input[3]
-      @pegs_array << "white"
-    end
-    if @secret_code[2] == @guess_input[0] || @secret_code[2] == @guess_input[1] || @secret_code[2] == @guess_input[3]
-      @pegs_array << "white"
-    end
-    if @secret_code[3] == @guess_input[0] || @secret_code[3] == @guess_input[1] || @secret_code[3] == @guess_input[2]
-      @pegs_array << "white"
+    # require "pry"; binding.pry
+    #XX @secret_code.each_char do |char|
+    #   (0..3).each do |index|
+    #     if char == @guess_input[index]
+    #       @pegs_array << "white"
+    #       break
+    #     end
+    #   end
+    # end
+    #XX (1..3)each do |index|
+    #   @secret_code[0] == @guess_input[1] || @secret_code[0] == @guess_input[2] || @secret_code[0] == @guess_input[3]
+    # end
+    #
+    @white = (0..3).count do |index1|
+      (0..3).any? do |index2|
+        @secret_code[index1] == @guess_input[index2] && index1 != index2
+      end
     end
   end
 
-  # def check_solution    ##MOVED TO GAME.rb
-  #   @guess_input == @secret_code
-  # end
-
   def get_guess
-    p "Please enter a guess. If you want to quit, press 'q' or press 'c' for the solution."
-    @guess_input = gets.chomp
-    @turn_counter += 1
-    quit if @guess_input == "q"
-    show_cheat_answer if @guess_input == "c"
-    # game.generate_solution    ##MOVED TO GAME.rb
-    # check_solution
-    # valid_input?
-    # zero_correct
-    # check_positions_colors
+    puts Rainbow("Please enter a guess (4 letters). If you want to quit, press 'q' or press 'c' for the solution.").lightskyblue.bold
+    @guess_input = gets.chomp.upcase
+    @counter += 1
+    puts Rainbow("Invalid input, please enter a four letter string.").papayawhip.bold if @guess_input.class != String || @guess_input.length != 4
+    puts "\n\n"
+    if @guess_input == "Q"
+      puts Rainbow("You are now leaving the game...\n\n\n\n\n\n\n\n").orange.bold.blink
+      exit
+    end
+    show_cheat_answer if @guess_input == "C"
   end
 
   def show_cheat_answer
-    p "The secret code is #{@secret_code}."
-    # quit  ##NO call reverses
+    puts Rainbow("The secret code was #{@secret_code}. You are now leaving the game...\n\n\n\n\n\n\n\n").cyan.bold.blink
+    exit
   end
 
   def show_guess_results
-    # require "pry"; binding.pry
-    #clear screen
-    p `clear`
-    p "#{@guess_input} has #{@pegs_array.count('white').to_s} of the correct elements with #{@pegs_array.count('red').to_s} in the correct positions."
-    p "You've taken #{@turn_counter} guess."
+    puts "\e[2J\e[f"
+    puts Rainbow("               <><><><>   MASTERMIND   <><><><>\n").rebeccapurple.bold.blink
+    @results_array.each do |result|
+      puts result
+    end
+    puts ""
+    puts Rainbow("#{@guess_input} has #{@pegs_array.count('white')} of the correct elements with #{@pegs_array.count('red')} in the correct positions.").lightskyblue
+    puts Rainbow(" => You've taken #{@counter} guess(es).\n\n").lightskyblue
+    @results_array << " |  TURN ##{@counter}: Guess '#{guess_input}' had *#{@pegs_array.count('white')} Elements & *#{@pegs_array.count('red')} Positions correct.  | "
   end
 
-  def valid_input?
-    #check quess.length == 4 && correct letter
-    if @guess_input.length != 4
-      p "Please insert a valid guess."
-      get_guess
-    else
-      check_positions_colors
-    end
-  end
-
-  def zero_correct
-    #first check, if no match, go direct to next guess
-    if @secret_code.include?(@guess_input[0]) == false && @secret_code.include?(@guess_input[1]) == false && @secret_code.include?(@guess_input[2]) == false && @secret_code.include?(@guess_input[3]) == false
-      p "You got zero correct, please guess again."
-      @turn_counter += 1
-      get_guess
-    #   break
-    end
-  end
 end
