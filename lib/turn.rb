7980faa11ae1a1dd92a72f1a575c3923ef276707
Klaudia Stewart <klaudiastewart@gmail.com>
2021-02-16 21:10:03 -0700

    Add tests to turn_test

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,35 +2,21 @@ require 'rainbow'
 require 'colorize'
 
 class Turn
-  attr_reader :pegs_array, :results_array
+  attr_reader :red_count, :white_count
+
+  @@results_array = [] #this is a class variable, to call this on another class, Turn.results_array
 
   def initialize(secret_code, guess_input)
     @secret_code = secret_code
     @guess_input = guess_input
-    # @counter = 0
-    @pegs_array = []
-    @results_array = []
+    @white_count = 0
+    @red_count = 0
   end
 
   def check_positions_colors(guess_input)
-    @pegs_array = []  #reset
-    (0...@secret_code.length).each do |index|
-      @pegs_array << "red" if @secret_code[index] == @guess_input[index]
+    @red_count = (0...@secret_code.length).count do |index|
+      @secret_code[index] == @guess_input[index]
     end
-    # require "pry"; binding.pry
-    #XX @secret_code.each_char do |char|
-    #   (0..3).each do |index|
-    #     if char == @guess_input[index]Copyright (c) 2018 Copyright Holder All Rights Reserved.
-    #       @pegs_array << "white"
-    #       break
-    #     end
-    #   end
-    # end
-    #XX (1..3)each do |index|
-    #   @secret_code[0] == @guess_input[1] || @secret_code[0] == @guess_input[2] || @secret_code[0] == @guess_input[3]
-    # end
-    #
-
     @white_count = (0...@secret_code.length).count do |index1|
       (0...@secret_code.length).any? do |index2|
         @secret_code[index1] == @guess_input[index2] && index1 != index2 && @secret_code[index1] != @guess_input[index1]
@@ -57,12 +43,12 @@ class Turn
     puts "\e[2J\e[f"
     puts Rainbow("                     <><><><>   MASTERMIND   <><><><>").rebeccapurple.bold.blink
     puts " _______________________________________________________________________\n\n"
-    @results_array.each do |result|
+    @@results_array.each do |result|
       puts result
     end
     puts " _______________________________________________________________________\n\n"
-    puts Rainbow("  TURN ##{turn_counter}: Guess '#{@guess_input}'' has #{@white_count} of the correct elements with #{@pegs_array.count('red')} in the correct positions.").lightskyblue
+    puts Rainbow("  TURN ##{turn_counter}: Guess '#{@guess_input}'' has #{@white_count} of the correct elements with #{@red_count} in the correct positions.").lightskyblue
     puts Rainbow("     => You've taken _ #{turn_counter} _ guess(es).\n\n").lightskyblue
-    @results_array << "   TURN ##{turn_counter}: Guess '#{@guess_input}' had correct -- *#{@white_count} Elements & *#{@pegs_array.count('red')} Positions."
+    @@results_array << "   TURN ##{turn_counter}: Guess '#{@guess_input}' had correct -- *#{@white_count} Elements & *#{@red_count} Positions."
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,25 +4,195 @@ require './lib/game'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
-  def setup
-    @turn = Turn.new(secret_code = nil)
-    @game = Game.new
-  end
-
   def test_turn_exists
-    assert_instance_of Turn, @turn
+    turn = Turn.new(@secret_code, @guess_input)
+
+    assert_instance_of Turn, turn
   end
 
-  def test_turn_is_zero_at_start
-    assert_equal 0, @turn.counter
+  def test_red_count_empty_at_start
+    turn = Turn.new(@secret_code, @guess_input)
+
+    assert_equal 0, turn.red_count
   end
 
-  def test_turn_can_add
-    @game.get_guess
-    assert_equal 1, @turn.get_counter
+  def test_for_no_red_or_white_peg_in_four_letter_code
+    secret_code = "RRRR"
+    guess_input = "BBBB"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 0, turn.red_count
+    assert_equal 0, turn.white_count
   end
 
-  def test_pegs_array_empty_at_start
-    assert_equal [], @turn.pegs_array
+  def test_for_one_red_peg_in_four_letter_code
+    secret_code = "RRRR"
+    guess_input = "RBBB"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 1, turn.red_count
   end
+
+  def test_for_all_four_red_pegs_in_four_letter_code
+    secret_code = "RRRR"
+    guess_input = "RRRR"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 4, turn.red_count
+  end
+
+  def test_for_no_white_peg_in_four_letter_code
+    secret_code = "RRRR"
+    guess_input = "BBBB"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 0, turn.white_count
+  end
+
+  def test_one_white_peg_in_four_letter_code
+    secret_code = "YYYR"
+    guess_input = "RBBB"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 1, turn.white_count
+  end
+
+  def test_four_white_pegs_in_four_letter_code
+    secret_code = "RYBG"
+    guess_input = "GBYR"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 4, turn.white_count
+  end
+
+  def test_for_two_red_two_white_pegs_in_four_letter_code
+    secret_code = "RYBR"
+    guess_input = "RBYR"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 2, turn.white_count
+    assert_equal 2, turn.red_count
+  end
+
+  def test_for_one_red_three_white_pegs_in_four_letter_code
+    secret_code = "RYBG"
+    guess_input = "RBGY"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 1, turn.red_count
+    assert_equal 3, turn.white_count
+  end
+
+  def test_for_zero_red_three_white_pegs_in_four_letter_code
+    secret_code = "GYBY"
+    guess_input = "RGYB"
+    turn = Turn.new(secret_code, guess_input)
+
+    turn.check_positions_colors(guess_input)
+
+    assert_equal 0, turn.red_count
+    assert_equal 3, turn.white_count #game shows 4
+  end
+
+  # def test_for_no_red_or_white_peg_in_six_letter_code
+  #   secret_code = "RRRRRR"
+  #   guess_input = "BBBBBB"
+  #   turn = Turn.new(secret_code, guess_input)
+  #
+  #   turn.check_positions_colors(guess_input)
+  #
+  #   assert_equal 0, turn.red_count
+  #   assert_equal 0. turn.white_count
+  # end
+  #
+  # def test_for_one_red_peg_in_six_letter_code
+  #   secret_code = "RRRR"
+  #   guess_input = "RBBB"
+  #   turn = Turn.new(secret_code, guess_input)
+  #
+  #   turn.check_positions_colors(guess_input)
+  #
+  #   assert_equal "red", turn.red_count[0]
+  # end
+  #
+  # def test_for_all_four_red_pegs_in_six_letter_code
+  #   secret_code = "RRRR"
+  #   guess_input = "RRRR"
+  #   turn = Turn.new(secret_code, guess_input)
+  #
+  #   turn.check_positions_colors(guess_input)
+  #
+  #   assert_equal "red", turn.red_count[0]
+  #   assert_equal "red", turn.red_count[1]
+  #   assert_equal "red", turn.red_count[2]
+  #   assert_equal "red", turn.red_count[3]
+  # end
+  #
+  # def test_for_no_white_peg_in_six_letter_code
+  #   secret_code = "RRRR"
+  #   guess_input = "BBBB"
+  #   turn = Turn.new(secret_code, guess_input)
+  #
+  #   turn.check_positions_colors(guess_input)
+  #
+  #   assert_equal 0, turn.white_count
+  # end
+  #
+  # def test_one_white_peg_in_six_letter_code
+  #   secret_code = "YYYR"
+  #   guess_input = "RBBB"
+  #   turn = Turn.new(secret_code, guess_input)
+  #
+  #   turn.check_positions_colors(guess_input)
+  #
+  #   assert_equal 1, turn.white_count
+  # end
+  #
+  # def test_four_white_pegs_in_six_letter_code
+  #   secret_code = "RYBG"
+  #   guess_input = "GBYR"
+  #   turn = Turn.new(secret_code, guess_input)
+  #
+  #   turn.check_positions_colors(guess_input)
+  #
+  #   assert_equal 4, turn.white_count
+  # end
+  #
+  # def test_for_two_red_two_white_pegs_in_six_letter_code
+  #   secret_code = "RYBR"
+  #   guess_input = "RBYR"
+  #   turn = Turn.new(secret_code, guess_input)
+  #
+  #   turn.check_positions_colors(guess_input)
+  #
+  #   assert_equal 2, turn.white_count
+  #   assert_equal 2, turn.red_count.size
+  # end
+  #
+  # def test_for_one_red_three_white_pegs_in_six_letter_code
+  #   secret_code = "RYBG"
+  #   guess_input = "RBGY"
+  #   turn = Turn.new(secret_code, guess_input)
+  #
+  #   turn.check_positions_colors(guess_input)
+  #
+  #   assert_equal 1, turn.red_count.size
+  #   assert_equal 3, turn.white_count
+  # end
 end
